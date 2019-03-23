3e1e3d87bf86a70e0fd08d5d3abd186f5ece1955
Jose <jsfuentescastillo@gmail.com>
2019-03-23 19:19:23 +0100

    Implement auth flow

--- internal/oauth2/oauth2.go
+++ internal/oauth2/oauth2.go
@@ -2,6 +2,7 @@ package oauth2
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"os"
 
@@ -22,4 +23,34 @@ func ReadTokenFromFile(path string) (*oauth2.Token, error) {
 	tok := &oauth2.Token{}
 	err = json.NewDecoder(f).Decode(tok)
 	return tok, err
+}
+
+func RetrieveTokenInteractively(config *oauth2.Config) (*oauth2.Token, error) {
+	authURL := config.AuthCodeURL("state-token", oauth2.AccessTypeOffline)
+	fmt.Printf("Go to the following link in your browser then type the authorization code:\n\n%v\n", authURL)
+
+	fmt.Println("\nPaste here the authorization code:")
+
+	var authCode string
+	if _, err := fmt.Scan(&authCode); err != nil {
+		return nil, fmt.Errorf("Unable to read authorization code: %v", err)
+	}
+
+	token, err := config.Exchange(context.TODO(), authCode)
+	if err != nil {
+		return nil, fmt.Errorf("Unable to retrieve token from web: %v", err)
+	}
+
+	return token, nil
+}
+
+func WriteTokenToFile(token *oauth2.Token, path string) error {
+	fmt.Printf("Saving credential file to: %s\n", path)
+	f, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
+	if err != nil {
+		return fmt.Errorf("Unable to cache oauth token: %v", err)
+	}
+	defer f.Close()
+	json.NewEncoder(f).Encode(token)
+	return nil
 }
--- main.go
+++ main.go
@@ -20,6 +20,56 @@ var auth bool
 // SheetIDFromURL is a regexp to extract Google sheetID from url
 var SheetIDFromURL = regexp.MustCompile(`https://docs.google.com/spreadsheets/d/(?P<id>.*)/.*`)
 
+func init() {
+	flag.StringVar(&configFile, "config", "./config.json", "Path to the config file.")
+	flag.StringVar(&userTokenFile, "userTokenFile", "./token.json", "Path to a file with user oauth2 token.")
+	flag.BoolVar(&auth, "auth", false, "Triggers the auth workflow.")
+	flag.Parse()
+}
+
+func main() {
+
+	configReader, err := os.Open(configFile)
+	if err != nil {
+		glog.Fatalf("%+v", err)
+	}
+
+	var config bdaybot.Config
+	jsonpb.Unmarshal(configReader, &config)
+
+	if auth {
+		glog.Info("Starting auth workflow...")
+		token, err := oauth2.RetrieveTokenInteractively(googlesheet.GenOauthConfig(
+			config.GetOauth2().GetClientId(),
+			config.GetOauth2().GetClientSecret(),
+		))
+		if err != nil {
+			glog.Fatalf("%v", err)
+		}
+
+		err = oauth2.WriteTokenToFile(token, userTokenFile)
+		if err != nil {
+			glog.Fatalf("%v", err)
+		}
+		os.Exit(0)
+	}
+
+	reader, err := genGooglesheetReader(config)
+	if err != nil {
+		glog.Fatalf("%+v", err)
+	}
+
+	bdays, err := reader.GetBdays()
+	if err != nil {
+		glog.Fatalf("%+v", err)
+	}
+
+	for _, bday := range bdays {
+		// TODO: is today? -> sent message in slack
+		fmt.Println(bday)
+	}
+}
+
 func genGooglesheetReader(config bdaybot.Config) (*googlesheet.Reader, error) {
 	oauthConfig := googlesheet.GenOauthConfig(config.GetOauth2().GetClientId(), config.GetOauth2().GetClientSecret())
 
@@ -46,41 +96,4 @@ func getSheetID(sheet *bdaybot.GoogleSheet) (string, error) {
 	}
 
 	return match[1], nil
-}
-
-func init() {
-	flag.StringVar(&configFile, "config", "./config.json", "Path to the config file.")
-	flag.StringVar(&userTokenFile, "userTokenFile", "./token.json", "Path to a file with user oauth2 token.")
-	flag.BoolVar(&auth, "auth", false, "Triggers the auth workflow.")
-	flag.Parse()
-}
-
-func main() {
-	if auth {
-		glog.Info("Starting auth workflow...")
-		os.Exit(0)
-	}
-
-	configReader, err := os.Open(configFile)
-	if err != nil {
-		glog.Fatalf("%+v", err)
-	}
-
-	var config bdaybot.Config
-	jsonpb.Unmarshal(configReader, &config)
-
-	reader, err := genGooglesheetReader(config)
-	if err != nil {
-		glog.Fatalf("%+v", err)
-	}
-
-	bdays, err := reader.GetBdays()
-	if err != nil {
-		glog.Fatalf("%+v", err)
-	}
-
-	for _, bday := range bdays {
-		// TODO: is today? -> sent message in slack
-		fmt.Println(bday)
-	}
 }
